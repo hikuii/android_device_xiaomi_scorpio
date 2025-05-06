9f67ff2b955c216933196bfde56160448fcafd71
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-06 15:24:50 +0545

    scorpio: Switch to common AIDL IR service
    Change-Id: I36c05aaa312969e479896c6e63a26a68cf897afa

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_COPY_FILES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir-service.xiaomi
+    android.hardware.ir-service.lineage
 
 # ConfigPanel
 PRODUCT_PACKAGES += \
